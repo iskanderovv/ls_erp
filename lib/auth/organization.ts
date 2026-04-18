1afefc94087152eeec3b0ca4d2d29cb5e618610c
Akbar Iskandarov <iskanderovofficial@gmail.com>
2026-04-18 22:04:14 +0500

    feat: handle potential undefined state of Prisma organization client in GET requests

--- lib/auth/organization.ts
+++ lib/auth/organization.ts
@@ -2,6 +2,9 @@ import { prisma } from "@/lib/prisma";
 import { SubscriptionPlan } from "@prisma/client";
 
 export async function getOrganization(id: string) {
+  // @ts-expect-error prisma.organization might be temporarily undefined
+  if (!prisma.organization) return null;
+  // @ts-expect-error prisma.organization
   return prisma.organization.findUnique({
     where: { id },
   });
